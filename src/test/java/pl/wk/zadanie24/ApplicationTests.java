dd374f99eeeb510c2830f1d875e9d6847c4bfca8
Wiktoria Kulczyńska <wiktoriakulczynska@gmail.com>
2022-01-05 00:35:53 +0100

    Otwarcie projektu

---
+++ src/test/java/pl/wk/zadanie24/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wk.zadanie24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
